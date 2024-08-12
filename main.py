659b3a594448ad01ddbda1b34f88cb6429871e5e
Marsel <kms-official@yandex.ru>
2024-08-13 03:57:20 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# Module 1 Practice 4
+print('Задание 1')
+my_string = input('Введите VIN транспортного средства: ')
+print('VIN ТС содержит',my_string.__len__(),'символов')
+print('Задание 2')
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
